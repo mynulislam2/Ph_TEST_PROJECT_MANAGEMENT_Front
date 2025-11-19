faee8d6273f97ef749b264737b01b10af2b8f467
Mynul-Dev <mynul.ir@tudotechnologies.com>
2025-11-19 18:22:35 +0600

    fixed baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
+const baseURL = 'https://ph-test-project-management-front.vercel.app/api';
 
 export const api = axios.create({ baseURL });
 
